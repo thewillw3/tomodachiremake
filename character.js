417354fb01f7da1bcddfc35b4481ab252d23730e
unknown <ytthewillw@gmail.com>
2025-01-16 22:48:56 -0500

    Why so serious...

--- character.js
+++ character.js
@@ -3,17 +3,20 @@ class Character {
     aliases;
     gender;
     relationships;
+    romanceable;
 
     /**
      * Constructor function for the Character class.
      * @param {string[]} cAliases - The list of the character's aliases: [firstName, lastName, nickName].
      * @param {Gender} cGender - The character's gender.
-     * @param {Map<string, number>} cRelationships - Map storing characters and their relationship scores.
+     * @param {Map<string, number>} cRelationships - Map storing characters and their relationship scores (Should be left empty).
+     * @param {Set<Gender>} cRomanceable - An set of genders that the character will date (Should be left empty).
      */
-    constructor(cAliases, cGender, cRelationships=new Map()) {
+    constructor(cAliases, cGender, cRelationships=new Map(), cRomanceable=new Set()) {
         this.aliases = cAliases;
         this.gender = cGender;
         this.relationships = cRelationships;
+        this.romanceable = cRomanceable;
     }
     
     /**
@@ -31,10 +34,23 @@ class Character {
 
     /**
      * This function returns the character's nickname.
-     * @returns {string} - string containing the character's nickname.
+     * @returns {string} - String containing the character's nickname.
      */
     nickName() {
         return this.aliases[2];
+    }
+
+    /**
+     * Function returns the relationship value between this character and the selected friend.
+     * @param {string} cKey - Key of the character in the character's friends list. 
+     * @returns - Nothing.
+     */
+    relationshipVal(cKey) {
+        if (!this.relationships.has(cKey)) {
+            console.log(`Character "${this.aliases[0]}" does not know character "${`cKey`}".`);
+        }
+
+        return this.relationships.get(cKey);
     }
 
     /**
@@ -42,7 +58,8 @@ class Character {
      * @param {string} cKey - The key of the character to establish a relationship with. 
      */
     createRelationship(cKey) {
-        this.relationships.set(cKey, 0.5);
+        const startingValue = 0.5;
+        this.relationships.set(cKey, startingValue);
     }
 
     /**
@@ -58,6 +75,41 @@ class Character {
         }
         
         this.relationships.delete(cKey);
+    }
+
+    /**
+     * This function modifies the relationship value with another character.
+     * @param {string} cKey - The character to modify the relationship with.
+     * @param {number} cRelChange - The amount to change the relationship by.
+     * @returns - Nothing.
+     */
+    modifyRelationship(cKey, cRelChange) {
+       this.relationships.set(cKey, this.relationshipVal(cKey) + cRelChange);
+    }
+
+    /**
+     * This function allows the addition of genders this character is attracted to.
+     * @param {Gender} rGender - Gender that character should be able to romance.
+     */
+    addRomanceable(rGender) {
+        const genderName = rGender.name;
+        this.romanceable.add(genderName);
+    }
+
+    /**
+     * This function allows the removal of a gender this character is attracted to.
+     * @param {Gender} rGender - Gender to remove from the romanceable list.
+     * @returns - Nothing.
+     */
+    deleteRomanceable(rGender) {
+        const genderName = rGender.name;
+
+        if (!this.romanceable.has(genderName)) {
+            console.log(`"${genderName}" does not exist in the set.`);
+            return;
+        }
+
+        this.romanceable.delete(genderName);
     }
 }
 
